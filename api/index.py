485db572280ffa19eb34a375e7a915de4f481da7
azki <2407048@itg.ac.id>
2026-06-20 09:36:18 +0700

    Fix: Add sys.path to api/index.py for Vercel

--- api/index.py
+++ api/index.py
@@ -1,4 +1,11 @@
+import sys
+import os
+
+# PENTING: Tambahkan folder root project ke Python Path
+# Agar api/index.py bisa "melihat" folder app yang ada di luar
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
+
 from app import create_app
 
-# Vercel membutuhkan variabel 'application'
+# Vercel membutuhkan variabel bernama 'application'
 application = create_app()
